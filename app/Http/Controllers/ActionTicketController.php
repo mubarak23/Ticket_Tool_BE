9ccdc0123d98079c0ce28a2df66164d27072a560
mubarak23 <mubarakaminu340@gmail.com>
2020-11-25 17:19:49 +0100

    action list

--- app/Http/Controllers/ActionTicketController.php
+++ app/Http/Controllers/ActionTicketController.php
@@ -28,14 +28,15 @@ class ActionTicketController extends Controller
 
           $data = $request->all();
           //return $data["user_id"];
+          //return $data;
 
          $check_action_ticket = Action_Ticket::where('user_id', $data['user_id'])->where('case_id', $data['case_id'])->exists();
          if($check_action_ticket){
          	//run an update
 
          	$update_ticket_action = Action_Ticket::where('user_id', $data['user_id'])->where('case_id', $data['case_id'])->update(['actions' => $data['actions']]);
-         	
-         	return response()->json(['message' => 'Action on Ticket Updated Sucessfully'], 200);
+         	$message = "Action on Ticket Updated Sucessfully";
+         	return response()->json($message, 200);
          }
 
         $create_action_on_ticket= $ActionService->execute($data);
